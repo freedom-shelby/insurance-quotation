20ffea523bc329091b6246751d7b681b3dfb4e2e
Arsen Hovhannisyan <arsen.hovhannisyan.1991@gmail.com>
2026-05-28 00:33:32 +0400

    - Integrate the JWT auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,18 +1,25 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
+/**
+ * @method static create(mixed $validated)
+ * @method static firstOrCreate(string[] $array, string[] $array1)
+ * @property mixed $id
+ */
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -28,5 +35,16 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function getJWTIdentifier(): mixed
+    {
+        return $this->getKey();
+    }
+
+    /** @inheritDoc */
+    public function getJWTCustomClaims(): array
+    {
+        return [];
     }
 }
